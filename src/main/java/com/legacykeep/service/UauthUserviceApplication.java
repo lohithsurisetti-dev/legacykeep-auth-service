3484a11a1b874873dc459050ec205536a9ae49db
lohith <lohith@psense.ai>
2025-08-21 00:32:12 -0500

    Initial commit: auth-service microservice

---
+++ src/main/java/com/legacykeep/service/UauthUserviceApplication.java
@@ -0,0 +1,12 @@
+package com.legacykeep.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UauthUserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UauthUserviceApplication.class, args);
+    }
+}
